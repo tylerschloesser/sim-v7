1de04bee5819ed589e386f2e7dbde57a3236826a
Tyler Schloesser <tylerschloesser@gmail.com>
2024-05-20 08:24:57 -0700

    add background

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -2,5 +2,9 @@ export function App() {
   const width = window.innerWidth
   const height = window.innerHeight
 
-  return <svg width={width} height={height} />
+  const viewBox = `0 0 ${width} ${height}`
+
+  return (
+    <svg width={width} height={height} viewBox={viewBox} />
+  )
 }
