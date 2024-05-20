f621b6e09225088737b6944b234351710e37b31f
Tyler Schloesser <tylerschloesser@gmail.com>
2024-05-20 08:25:37 -0700

    render circle

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -5,6 +5,13 @@ export function App() {
   const viewBox = `0 0 ${width} ${height}`
 
   return (
-    <svg width={width} height={height} viewBox={viewBox} />
+    <svg width={width} height={height} viewBox={viewBox}>
+      <circle
+        cx={width / 2}
+        cy={height / 2}
+        r={Math.min(width, height) * 0.2}
+        fill="blue"
+      />
+    </svg>
   )
 }
