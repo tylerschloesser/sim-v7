fa80bf366904a20bc003af63b63044011fa0d254
Tyler Schloesser <tylerschloesser@gmail.com>
2024-05-25 20:15:13 -0700

    fix index

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
 import { App } from './app'
-import { AppContext, IAppContext } from './app-context'
+import { AppContext } from './app-context'
 
 const container = document.getElementById('app')
 invariant(container)
 
 const root = createRoot(container)
 
-const context: IAppContext = {
+const context: AppContext = {
   vw: window.innerWidth,
   vh: window.innerHeight,
 }
